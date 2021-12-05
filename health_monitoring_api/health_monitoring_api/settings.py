7af3825d3644c8a041ccbcd9aacaed893aff74d8
MelahGhislain <melahghislain17@gmail.com>
2021-12-05 10:09:19 +0100

    setup JWT

--- health_monitoring_api/health_monitoring_api/settings.py
+++ health_monitoring_api/health_monitoring_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -98,7 +100,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+# Django rest framework
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+   
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
