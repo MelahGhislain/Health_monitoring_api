9132031ddcb5a0071457c26e831be52466d4f8dc
MelahGhislain <melahghislain17@gmail.com>
2021-12-06 16:48:17 +0100

    updates

--- health_monitoring_api/user_auth/serializers.py
+++ health_monitoring_api/user_auth/serializers.py
@@ -6,17 +6,18 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 # Register serializer
 class RegisterSerializer(serializers.ModelSerializer):
+    # confirm_password = serializers.StringRelatedField(write_only=True)
     class Meta:
         model = User
-        fields = ('id','username','password','first_name', 'last_name')
+        fields = ('id','username','email','password',)
         extra_kwargs = {
             'password':{'write_only': True},
         }
         def create(self, validated_data):
+
             user = User.objects.create_user(validated_data['username'], 
                         password = validated_data['password'],
-                        first_name=validated_data['first_name'],  
-                        last_name=validated_data['last_name'])
+                        email = validated_data['email'],)
             return user
 
 # User serializer
